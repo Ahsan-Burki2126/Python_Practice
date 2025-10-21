dc02fd0dc49bd10c6e8edaf1b363a789c77d584a
Ahsan Burki <ahsanburki2126@gmail.com>
2025-10-21 13:44:12 +0500

    Monthly expense challenge completed

--- Day-01/monthly_expense.py
+++ Day-01/monthly_expense.py
@@ -1,6 +1,23 @@
 #Your monthly expense list (from Jan to May) looks like this,
-expense_list = [2340, 2500, 2100, 3100, 2980]
 """
 Write a program that asks you to enter an expense amount and program should tell you in which month that expense occurred. If expense is not found then it should print that as well.
 """
+expense_list = [2340, 2500, 2100, 3100, 2980]
+months = ["january","Feburary","March","April","May"]
+
+
+def find_expense(exp):
+    expense_to_found = exp
+    try:
+        indexOfExpense=expense_list.index(expense_to_found)
+        print(f"{expense_to_found} is the expense for {months[indexOfExpense]}")
+    except ValueError:
+        print(f"the value you entered {expense_to_found} is not in the list")    
+
+
+
+
+find_expense(2980)
+
+
 
